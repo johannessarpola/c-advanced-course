2677feee6e828d1265fdf9d1023b3b543d8589b7
Johannes Sarpola <johannes.sarpola@gmail.com>
2023-06-27 14:24:37 +0300

    register

--- src/register/register.c
+++ src/register/register.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    // Register can only be used in local scope and not in global scope
     // X is stored in a register, not RAM, does not have memory address (can't use &)
     register int x;
     for (x = 0; x <= 15; x++)
     {
         printf("%d\n", x);
-        /* code */
     }
+
+    // Register can store pointer
+    int y = 99;
+    register int *ptr_y = &y;
+    printf("%p: %d\n", ptr_y, *ptr_y);
+
     return 1;
 }
