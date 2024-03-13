24689e26a5e83eca7e314dd4a6ba019cceeeaf45
Johannes Sarpola <johannes.sarpola@gmail.com>
2024-03-13 16:51:05 +0200

    test dynlib

---
+++ src/section_18/dynamic lib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main() {
+
+    func();
+
+    return 0;
+}
---
+++ src/section_18/dynamic lib/mylib.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void func(void) {
+    printf("in the dynamic library now");
+}
