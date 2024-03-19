441c5e7a44386fc20dd40c00db25688853ade57b
Johannes Sarpola <johannes.sarpola@qalmari.fi>
2024-03-19 17:06:30 +0200

    add simple tcp server

---
+++ src/section_23/tcp_server/client.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main()
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char read_buffer[BUFFER_SIZE] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) // SOCK_STREAM === TCP
+    {
+        perror("socket creation failed");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    // we dont need address.sin_addr.s_addr as s_addr reference to listen address
+    serv_addr.sin_port = htons(PORT);
+
+    // inet_pton - convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        perror("invalid address");
+        return -1;
+    }
+
+    // connect - initiate a connection on a socket
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("connection Failed");
+        return -1;
+    }
+
+    int data_size = 20;
+    char *data = "Hello from client";
+
+    send(sock, data, data_size, 0);
+    printf("sent message to the server\n");
+
+    valread = read(sock, read_buffer, BUFFER_SIZE);
+    printf("response (%d length) from server:\n%s", valread, read_buffer);
+    fflush(stdout);
+    close(sock);
+    return 0;
+}
---
+++ src/section_23/tcp_server/server.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+// Function to read the content of a file into a dynamically allocated buffer
+char *read_file(const char *filename, long *size) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    *size = ftell(file);
+    rewind(file);
+
+    char *content = (char *)malloc((*size + 1) * sizeof(char));
+    if (!content) {
+        fclose(file);
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
+    fread(content, sizeof(char), *size, file);
+    content[*size] = '\0'; // Null-terminate the content
+    fclose(file);
+    return content;
+}
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char *html_content;
+    long html_size;
+
+    // Read the content of index.html
+    html_content = read_file("./index.html", &html_size);
+    if (!html_content) {
+        exit(EXIT_FAILURE);
+    }
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { // SOCK_STREAM === TCP
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET; // basic internet family
+    address.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
+    address.sin_port = htons(PORT); // @manpages: function converts the unsigned short integer hostshort from host byte order to network byte order.
+
+    // Bind socket to address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        // create new socket from connections accepted
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // echo request details
+        read(new_socket, buffer, BUFFER_SIZE);
+        printf("Request: %s\n", buffer);
+
+        // add basic headers with dprintf
+        dprintf(new_socket, "HTTP/1.1 200 OK\r\n");
+        dprintf(new_socket, "Content-Type: text/html\r\n");
+        dprintf(new_socket, "Content-Length: %ld\r\n\r\n", html_size);
+        
+        // send te file contents
+        send(new_socket, html_content, html_size, 0);
+        printf("Response sent\n");
+
+        close(new_socket);
+    }
+
+    // Free allocated memory
+    free(html_content);
+
+    return 0;
+}
